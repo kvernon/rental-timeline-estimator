165230ed59268168b9ec74a3a4d3d3c5a4a9a681
kvernon <kvernon@cubedelement.com>
2023-10-06 20:43:46 -0500

    chore: dep update

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,19 +4,23 @@ module.exports = {
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
+
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions"
   ],
-  "framework": "@storybook/react",
-  core: {
-    builder: 'webpack5',
+
+  "framework": {
+    name: '@storybook/react-webpack5',
+    options: {}
   },
+
   features: {
     emotionAlias: false,
     storyStoreV7: true,
   },
+
   webpackFinal: async (config) => {
     config.plugins.push(    new CopyPlugin({
         patterns: [{ from: 'images', to: 'images' }]
@@ -26,4 +30,8 @@ module.exports = {
     // Return the altered config
     return config;
   },
+
+  docs: {
+    autodocs: true
+  }
 }
