6f05e7c4a3b529617ed4e069e52333059e51df22
kvernon <kvernon@cubedelement.com>
2024-07-24 20:56:15 -0500

    fix: restore spinnner

---
+++ src/components/core/Spinner.spec.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { configure, render, screen } from '@testing-library/react';
+import { Spinner } from './Spinner';
+
+describe('Spinner unit tests', () => {
+  beforeEach(() => {
+    configure({ testIdAttribute: 'id' });
+  });
+  test('should render with shape', () => {
+    render(<Spinner role="spinner" shape={{ x: 1, height: 20, width: 30, y: 2 }} />);
+
+    const entity = screen.getByRole<HTMLDivElement>('spinner');
+
+    expect(entity).toMatchSnapshot();
+  });
+  test('should render without shape', () => {
+    render(<Spinner role="spinner" shape={{ x: undefined, height: undefined, width: undefined, y: undefined }} />);
+
+    const entity = screen.getByRole<HTMLDivElement>('spinner');
+
+    expect(entity).toMatchSnapshot();
+  });
+});
