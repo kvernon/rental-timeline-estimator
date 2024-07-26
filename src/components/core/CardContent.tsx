ee675e5309dec0a2e9deedff2af20c66910daaea
kvernon <kvernon@cubedelement.com>
2024-07-25 21:05:47 -0500

    fix: restore page; type; card; header

---
+++ src/components/core/CardContent.tsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+export const CardContent = styled.div`
+  padding: 0;
+  margin-top: 0;
+
+  display: block;
+
+  &:last-child {
+    padding-bottom: 0;
+  }
+`;
