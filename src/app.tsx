3f6573a050a9ebfe2bc48bba73c97c7410b0e3a3
kvernon <kvernon@cubedelement.com>
2024-07-26 06:57:14 -0500

    fix: update app

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { ThemeProvider } from '@emotion/react';
 import { options } from './theming/theme';
+import { Page } from './components/core/Page';
 
 export const App = function () {
   return (
     <ThemeProvider theme={options}>
+      <Page />
       <div>hi</div>
     </ThemeProvider>
   );
