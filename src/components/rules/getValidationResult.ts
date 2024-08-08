d1554ec762311e16f143ec50df2791aee0ac1a62
kvernon <kvernon@cubedelement.com>
2024-08-07 20:27:20 -0500

    feat: version 3 getValidationResult

---
+++ src/components/rules/getValidationResult.ts
@@ -0,0 +1,16 @@
+import { ValidatorTypes } from '../validators/ValidatorTypes';
+
+export const getValidationResult = (validations: ValidatorTypes[], isRequired: boolean): ValidatorTypes => {
+  if (!validations || validations.length === 0) {
+    return isRequired ? ValidatorTypes.Invalid : ValidatorTypes.Optional;
+  }
+
+  if (validations.some((x) => x === ValidatorTypes.Invalid || (x === ValidatorTypes.Optional && isRequired))) {
+    return ValidatorTypes.Invalid;
+  }
+  if (validations.every((x) => x === ValidatorTypes.Optional)) {
+    return ValidatorTypes.Optional;
+  }
+
+  return ValidatorTypes.Valid;
+};
