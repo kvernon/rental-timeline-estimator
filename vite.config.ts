6f506bb986b6b5bae121bde2371af2ed695f9d61
Kelly Vernon <kvernon@cubedelement.com>
2025-11-14 09:30:31 -0600

    feat: rit library update (#118)

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import pkg from './package.json';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,10 +13,21 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          appVersion: pkg.version,
+        },
+      },
+    }),
     viteStaticCopy({
       targets: [
         { src: 'images/*', dest: './images' }, // relative to outDir
       ],
     }),
   ],
+  build: {
+    sourcemap: true,
+    minify: 'esbuild',
+  },
 });
