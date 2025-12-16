fb1c62ffafcc65a355770bc2b3241a0f6e15cd19
kvernon <kvernon@cubedelement.com>
2025-12-16 13:15:06 -0600

    chore: progress check

--- src/components/timeline/StackSpaceBetween.tsx
+++ src/components/timeline/StackSpaceBetween.tsx
@@ -2,5 +2,5 @@ import styled from '@emotion/styled';
 import { Stack } from '../core/Stack';
 
 export const StackSpaceBetween = styled(Stack)`
-  justify-content: space-between;
+  justify-content: left;
 `;
